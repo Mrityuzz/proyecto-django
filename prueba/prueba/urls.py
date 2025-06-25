e703945a1a774e17014c2da2a64e2aef585f98cc
Your Name <macielbrayn@gmail.com>
2025-06-24 18:41:31 -0600

    Phyton GOD

--- prueba/prueba/urls.py
+++ prueba/prueba/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from inicio import views
 from django.conf import settings
+from registros import views as views_registros
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views_registros.registros, name="Prinicipal"),
     path('', views.principal, name='Principal'),
     path('contacto/', views.contacto, name='Contacto'),
     path('formulario/', views.formulario, name='Formulario'),
@@ -31,3 +33,4 @@ if settings.DEBUG:
     from django.conf.urls.static import static
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
--- prueba/registros/views.py
+++ prueba/registros/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Alumnos
 
 # Create your views here.
+
+def registros(request):
+    alumnos=Alumnos.objects.all()
+    return render(request, "registros/principal.html", {'alumnos': alumnos})
+#Indicamos el lugar donde se renderizará el resultado deesta vista
